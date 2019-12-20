247f93d2fa832a7e3460daf3549d6e2498f95e96
james <uptownhr@gmail.com>
2019-12-20 04:26:40 -0800

    added documentation decorators

--- src/modules/Todo/Todo.controller.ts
+++ src/modules/Todo/Todo.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TodoService, Todo } from './Todo.service';
+import { ApiTags, ApiOkResponse } from '@nestjs/swagger';
 
+@ApiTags('todos')
 @Controller('todos')
 export class TodoController {
   constructor(private readonly TodosService: TodoService) {}
 
+  @ApiOkResponse({
+    type: [Todo]
+  })
   @Get()
   async getTodos(): Promise<Todo[] | null> {
     return await this.TodosService.findAll();
--- src/modules/Todo/Todo.model.ts
+++ src/modules/Todo/Todo.model.ts
@@ -1,10 +1,16 @@
 import { prop } from '@typegoose/typegoose';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class Todo {
+  @ApiProperty()
   @prop({enum: ['confirmation'], default: 'confirmation'})
   type?: string;
+
+  @ApiProperty()
   @prop({ required: true })
   name: string;
+
+  @ApiProperty()
   @prop({
     enum: ['active', 'complete', 'cancelled'],
     default: 'active'
