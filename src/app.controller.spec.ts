5da347b9d99ae41fbedf585c7a15cb6e585e3f42
james <uptownhr@gmail.com>
2019-12-21 20:04:49 -0800

    fixed typo

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let app: TestingModule
+  let app: TestingModule;
   let appController: AppController;
 
   beforeEach(async () => {
