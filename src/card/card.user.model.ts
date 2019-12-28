60f8e58d6cf6c8e7c2377cb4a33768f77b59a89a
james <uptownhr@gmail.com>
2019-12-28 16:19:15 -0700

    added unique index for card, user

--- src/card/card.user.model.ts
+++ src/card/card.user.model.ts
@@ -1,9 +1,10 @@
-import { prop, Ref } from '@typegoose/typegoose';
+import { index, prop, Ref } from '@typegoose/typegoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { Schema } from 'mongoose';
 import { User } from '../user/user.model';
 import { Card } from './card.model';
 
+@index({user: 1, card: 1}, {unique: true})
 export class CardUser {
   @ApiProperty({type: String})
   @prop({required: true, ref: 'User'})
